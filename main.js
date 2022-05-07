8cc22ebcbc8bb35fc05d99b59ce5027d6e4df119
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-07 12:19:00 -0300

    Menu abrindo e fechando

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function onScroll() {
     scroll.classList.remove("scroll");
   }
 }
+
+function openMenu() {
+  document.body.classList.add("menu-active");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-active");
+}
